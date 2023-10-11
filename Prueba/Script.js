f7fe1b98ee100c365753a96e4dd7165c5c418ceb
RobertoDeLaPrida <rdelpul993@gmail.com>
2023-10-11 14:18:29 +0200

    11/10

--- Prueba/Script.js
+++
@@ -1,2 +0,0 @@
-
-document.getElementById("UwU").innerHTML="Prueba";
--- Prueba2/script.js
+++
@@ -1,8 +0,0 @@
-function procesarFormulario() {
-    const num = formNumero.Numero.value;
-    let salida ="";
-    for(let i=1;i<10;i++){
-        salida+=num+ " x " +i+" = "+num*i+"<br>";
-    }   
-    document.getElementById("tabla").innerHTML = salida;
-}
