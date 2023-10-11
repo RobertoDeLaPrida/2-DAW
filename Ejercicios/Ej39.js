f75a10ccac6df9f2598153a1e80a8dd90ca34009
RobertoDeLaPrida <rdelpul993@gmail.com>
2023-10-11 14:44:46 +0200

    Ej39

--- Ejercicios/Ej39.js
+++ Ejercicios/Ej39.js
@@ -1,6 +1,23 @@
 //Realizar un programa que liste los 100 primeros números primos.
-for (let i=0;i<100;i++){
-    if(i%=0){
-        document.getElementById("salida").innerHTML=i
+function esPrimo(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+  
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
     }
-}
+  
+    return true;
+  }
+  
+  const numerosPrimosMenoresDe100 = [];
+  
+  for (let i = 2; i < 100; i++) {
+    if (esPrimo(i)) {
+      numerosPrimosMenoresDe100.push(i);
+    }
+  }
+  
+  console.log(numerosPrimosMenoresDe100);
+  
