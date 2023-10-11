10985855cc829b98298df6ea9b9bb53c1b006346
RobertoDeLaPrida <rdelpul993@gmail.com>
2023-10-11 14:47:52 +0200

    Ej39

--- Ejercicios/Ej39.js
+++ Ejercicios/Ej39.js
@@ -1,24 +1,29 @@
 //Realizar un programa que liste los 100 primeros números primos.
 function esPrimo(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 === 0 || num % 3 === 0) return false;
-  
-    for (let i = 5; i * i <= num; i += 6) {
-      if (num % i === 0 || num % (i + 2) === 0) return false;
-    }
-  
-    return true;
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
   }
-  
-  const numerosPrimosMenoresDe100 = [];
-  
-  for (let i = 2; i < 100; i++) {
-    if (esPrimo(i)) {
-      numerosPrimosMenoresDe100.push(i);
+
+  return true;
+}
+
+function obtenerPrimerosPrimos(cantidad) {
+  const primerosPrimos = [];
+  let num = 2;
+
+  while (primerosPrimos.length < cantidad) {
+    if (esPrimo(num)) {
+      primerosPrimos.push(num);
     }
+    num++;
   }
-  
-  console.log(numerosPrimosMenoresDe100);
-  
-  document.getElementById("salida").innerHTML = "Números primos menores de 100: " + numerosPrimosMenoresDe100.join(", ");
+
+  return primerosPrimos;
+}
+
+const primeros100Primos = obtenerPrimerosPrimos(100);
+document.getElementById("salida").innerHTML=primeros100Primos;
